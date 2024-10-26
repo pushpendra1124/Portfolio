5f565fbb1e5bc4ffc39024de087a3652ff86fbf7
Pushpendra Singh <148307565+pushpendra1124@users.noreply.github.com>
2024-10-26 12:48:17 +0530

    Update Nav.js

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -14,7 +14,7 @@ const Nav = () => {
       <li><Link className='text' to="Technologies">Technologies</Link></li>
       <li><Link className='text' to="Contact">Contact</Link></li>
       
-      <li ><a href='https://github.com/govind124'><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" height={"34px"} /></a></li>
+      <li ><a href='https://github.com/pushpendra1124'><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="" height={"34px"} /></a></li>
       <li className='Linkdin'><a href='https://www.linkedin.com/in/pushpendra-singh-931b2019b/'><img src="https://www.iconpacks.net/icons/1/free-linkedin-icon-112-thumb.png" alt="" height={"30px"} /></a></li>
      
       <li className='insta'><a href="https://www.instagram.com/govind_.001/"><img src="https://www.pngitem.com/pimgs/m/1-14415_instagram-logo-black-borders-png-transparent-background-instagram.png" alt="" height={"30px"}/></a></li>
